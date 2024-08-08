65d94d217aca504c4da3a425b1b6210254c2ab68
Brdly777 <cristianbe2705@gmail.com>
2024-08-07 18:19:47 -0600

    Brdly keymap uploaded

--- keyboards/lily58/config.h
+++ keyboards/lily58/config.h
@@ -27,4 +27,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 
 // Use the lily version to get the Lily58 logo instead of the qmk logo
-#define OLED_FONT_H "lib/glcdfont_lily.c"
+#define OLED_FONT_H "keyboards/lily58/keymaps/brdly/glcdfont.c"
+
